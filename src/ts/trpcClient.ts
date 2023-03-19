8a069045f08047cf5b9ea7cd66f3a8a883fef98b
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-03-19 11:55:23 +0000

    fix: use origin instead of hardcoded URL

--- src/ts/trpcClient.ts
+++ src/ts/trpcClient.ts
@@ -1,14 +1,10 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from '../pages/api/trpc/[trpc]';
- 
-export const trpcClient = createTRPCProxyClient<AppRouter>({
+
+export const createTrpcClient = (serverUrl: string) => createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      // Warning: this probably needs to be updated when deployed. This only works in Cloudflare preview, since this URL
-      // is the temporary local preview IP address.
-      // Set this as an environment variable.
-      // TODO avoid passing the hostname, just use the same hostname
-      url: 'https://trpc-astro-cloudflare.pages.dev/api/trpc',
+      url: `${serverUrl}/api/trpc`,
     }),
   ],
 });

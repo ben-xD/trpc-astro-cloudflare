d013baeedae2bae6550dc8d91d7d38ec86d68775
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-04-02 22:37:53 +0100

    feat: add cloudflare bindings

--- src/pages/api/trpc/context.ts
+++ src/pages/api/trpc/context.ts
@@ -1,6 +1,11 @@
 import type { inferAsyncReturnType } from '@trpc/server';
 import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
 
+// Define binding to allow your worker to interact with Cloudflare D1.
+export interface Env {
+  DB: D1Database;
+}
+
 export function createContext({
   req,
   resHeaders,
